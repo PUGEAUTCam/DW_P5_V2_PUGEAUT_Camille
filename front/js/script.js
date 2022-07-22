998037bfe6c0a319e2fba4ddf82f875fc69ebe2e
PUGEAUT Camille <camille.pugeaut@gmail.com>
2022-07-22 11:59:20 +0200

    Etape 3 : Inserer les produits dans la page accueil OK

---
+++ front/js/script.js
@@ -0,0 +1,57 @@
+// Appelle a l'API 
+// recuperation dynamique des donnees a afficher pour implementer la page
+
+let url = 'http://localhost:3000/api/product/';
+
+
+
+fetch(url)
+    .then(response => response.json())
+
+    .then((data) => {
+
+        let content = ``;
+
+        for (let i = 0; i <data.length; i++) {
+
+            let idKanap = data[i]._id;
+            let imgKanap = data[i].imageUrl;
+            let imgAltKanap = data[i].altTxt;
+            let nameKanap = data[i].name;
+            let descriptionKanap = data[i].description;            
+
+            content += ` 
+                    <a href="./product.html?id=${idKanap}">
+                        <article>
+                            <img src="${imgKanap}" alt="${imgAltKanap}">
+                            <h3 class="productName">${nameKanap}</h3>
+                            <p class="productDescription">${descriptionKanap}</p>
+                        </article>
+                    </a>
+                    `
+        } 
+
+        document.querySelector('#items').innerHTML = content;
+
+    }).catch((err) => {
+
+        document.querySelector('#items').textContent = 'Oups ! La page que vous cherchez ne semble pas disponible. Nos canapés reviennent bientôt.';
+    
+    });
+
+   
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
